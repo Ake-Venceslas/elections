d3a397b8dd6dfd6caa8cbe7ce36f24531f3bfb4e
Ake-Venceslas <akevenceslas850@gmail.com>
2025-09-29 18:03:33 +0100

    two

--- src/ivote-backend/routes/authRoutes.js
+++ src/ivote-backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
 
 const router = express.Router();
 
